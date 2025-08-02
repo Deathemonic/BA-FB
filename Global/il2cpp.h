b6e936813b7dcb82023d9a81c9db02500d99ce0d
GitHub Action <action@github.com>
2025-08-02 10:00:56 +0000

    Update data dumps - 2025-08-02 10:00:56

---
+++ Global/il2cpp.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1844d87a53e26dafa25d3d2436eecf932e83a0bec39bf467012923c277f119d
+size 137606068
---
+++ Japan/il2cpp.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78161b88e8796fb9987f9a48c7ad6f31d70a6a159398b038159b3e10fd470161
+size 162372537
